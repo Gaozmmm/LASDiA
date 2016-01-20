85516b8366fdcb39190fc0cce346d7d85e41e2db
cicciodevoto <devotofrancesco@gmail.com>
2016-01-20 17:13:08 +0100

    Finalized the chi2 minimization

--- Liquid.py
+++ Liquid.py
@@ -35,6 +35,7 @@ from scipy import fftpack
 from scipy import signal
 from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 import numpy as np
 
 from modules.Utility import *
@@ -73,51 +74,102 @@ if __name__ == '__main__':
     calculation_index = np.where((Q>minQ) & (Q<=QmaxIntegrate))
     
     elementList = {"Ar":1}
-    s =  0.5
-    rho0 = 24.00
+    # test values
+    s = np.arange(0.2, 0.8, 0.1)
+    rho0 = np.arange(24, 28, 1)
+    
+    # real values
+    # s = np.arange(0.2, 0.8, 0.01)
+    # rho0 = np.arange(24.0, 28.0, 0.1)
+    
+    # best values
+    # s = np.array([0.57])
+    # rho0 = np.array([26.10])
+    
+    chi2 = np.zeros((rho0.size, s.size))
     
     # remember the electron unit in atomic form factor!!!
     fe_Q, Ztot = calc_eeff(elementList, Q)
     Iincoh_Q = calc_Iincoh(elementList, Q)
     J_Q = calc_JQ(Iincoh_Q, Ztot, fe_Q)
-    Sinf = calc_Sinf(elementList, fe_Q, Q, Ztot)
-    Isample_Q = calc_IsampleQ(I_Q, s, I_Qbkg)
-    
-    alpha = calc_alpha(J_Q, Sinf, Q, Isample_Q, fe_Q, Ztot, rho0, integration_index)
-    Icoh_Q = calc_Icoh(N, alpha, Isample_Q, Iincoh_Q)
-    
-    S_Q, S_Qs = calc_SQ(N, Icoh_Q, Ztot, fe_Q, Sinf, Q, min_index, max_index, calculation_index)
-    
-    
-    plt.figure(1)
-    #plt.plot(Q[validation_index], S_Q)
-    plt.plot(Q[validation_index], S_Qs)
-    plt.grid()
-    plt.show
-    
-    i_Q = calc_iQ(S_Q, Sinf)
-#    i_Q = calc_iQ(S_Qs, Sinf)
-    Qi_Q = Q[validation_index] * i_Q
-    
-    DeltaQ = np.diff(Q)
-    meanDeltaQ = np.mean(DeltaQ)
-    r = fftpack.fftfreq(Q[validation_index].size, meanDeltaQ)
-    mask = np.where(r>0)
-    
-    F_r = calc_Fr(r[mask], Q[integration_index], i_Q[integration_index])
-    
-    plt.figure(2)
-    plt.plot(r[mask], F_r)
-    plt.grid()
-    plt.show
-    
-    iteration = 4
-    rmin = 0.24
-    F_rInt = calc_optimize_Fr(iteration, F_r, rho0, i_Q[integration_index], Q[integration_index], Sinf, J_Q[integration_index], r[mask], rmin)
-    
-    plt.figure(2)
-    plt.plot(r[mask], F_rInt)
-    plt.show()
 
+    for i, val_rho0 in enumerate(rho0):
+        for j, val_s in enumerate(s):
+            # print(rho0[i], s[j])
+            Sinf = calc_Sinf(elementList, fe_Q, Q, Ztot)
+            Isample_Q = calc_IsampleQ(I_Q, s[j], I_Qbkg)
+        
+            alpha = calc_alpha(J_Q, Sinf, Q, Isample_Q, fe_Q, Ztot, rho0[i], integration_index)
+            Icoh_Q = calc_Icoh(N, alpha, Isample_Q, Iincoh_Q)
+            
+            S_Q, S_Qs = calc_SQ(N, Icoh_Q, Ztot, fe_Q, Sinf, Q, min_index, max_index, calculation_index)
+        
+            # plt.figure(1)
+            # plt.plot(Q[validation_index], S_Q)
+            # # plt.plot(Q[validation_index], S_Qs)
+            # plt.grid()
+            # plt.show()
+            
+            i_Q = calc_iQ(S_Q, Sinf)
+        #    i_Q = calc_iQ(S_Qs, Sinf)
+            Qi_Q = Q[validation_index] * i_Q
+            
+            DeltaQ = np.diff(Q)
+            meanDeltaQ = np.mean(DeltaQ)
+            r = fftpack.fftfreq(Q[validation_index].size, meanDeltaQ)
+            mask = np.where(r>0)
+            
+            F_r = calc_Fr(r[mask], Q[integration_index], i_Q[integration_index])
+            
+            # plt.figure(2)
+            # plt.plot(r[mask], F_r)
+            # plt.grid()
+            # plt.show
+            
+            iteration = 2
+            rmin = 0.24
+            F_rInt = calc_optimize_Fr(iteration, F_r, rho0[i], i_Q[integration_index], Q[integration_index], Sinf, J_Q[integration_index], r[mask], rmin)
+            
+            # plt.figure(2)
+            # plt.plot(r[mask], F_rInt)
+            # plt.show()
+            
+            maskInt = np.where((r>0) & (r < rmin))
+            rInt = r[maskInt]
+            Fintra_r = calc_Fintra(rInt)
+            deltaF_r = calc_deltaFr(F_rInt[maskInt], Fintra_r, rInt, rho0[i])
+            chi2[i][j] = simps(deltaF_r**2, rInt)
+        
+    # np.amin(chi2)
+    # minIndx = np.where(chi2 == chi2.min())
+    # print(minIndx)
+    # print(s[minIndx])
+    
+    # print(chi2)
+    # print(np.amin(chi2, axis=0))
+    # print(np.amin(chi2, axis=1))
+    # print(np.amin(chi2))
+    minIndxRho0, minIndxS = np.unravel_index(chi2.argmin(), chi2.shape)
+    # print(minIndxRho0, minIndxS)
+    print(chi2[minIndxRho0][minIndxS])
+    print(rho0[minIndxRho0], s[minIndxS])
+    
+    # print(rho0.shape)
+    # print(s.shape)
+    # print(chi2.shape)
+    
+    # x, y = np.meshgrid(s, rho0)
+    
+    # print(x.shape)
+    # print(y.shape)
+    
+    plt.figure(3)
+    # fig = plt.figure()
+    # ax = Axes3D(fig)
+    # ax.plot_surface(x, y, chi2, rstride=1, cstride=1, cmap='hot')
+    plt.contour(s, rho0, chi2)
+    # l = plt.clabel(c)
+    plt.show()
+    
 
     
