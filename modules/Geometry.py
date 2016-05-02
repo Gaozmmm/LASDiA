0c53fbeaa6b1c1a690a9677538d1dc2ae0398169
cicciodevoto <devotofrancesco@gmail.com>
2016-05-02 15:41:37 +0200

    Working on geometric corrections.

---
+++ modules/Geometry.py
@@ -0,0 +1,37 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2015-2016 European Synchrotron Radiation Facility
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Module containing the geometric correction: Diamond and Soller SLit corrections.
+
+The nomenclature and the procedure follow the article:
+Eggert et al. 2002 PRB, 65, 174105.
+
+For the functions arguments and the returns I followed this convetion for the notes:
+arguments: description - type
+returns: description - type.
+
+For the variables name I used this convention:
+if the variable symbolizes a mathematical function, its argument is preceded by an underscore: f(x) -> f_x
+otherwise it is symbolized with just its name.
+"""
+
+def diamond():
