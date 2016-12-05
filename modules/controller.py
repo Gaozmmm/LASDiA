842a3cf0c97197b74ef8e401e4e0ed10f710e6ab
cicciodevoto <devotofrancesco@gmail.com>
2016-12-05 11:38:15 +0100

    Delete for now the MIT Licence on the controller.

--- modules/controller.py
+++ modules/controller.py
@@ -1,25 +1,3 @@
-# The MIT License (MIT)
-
-# Copyright (c) 2015-2016 European Synchrotron Radiation Facility
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 """LASDiA Controller
 """
 
@@ -91,7 +69,9 @@ class LASDiA(QtWidgets.QMainWindow, LASDiAGUI.Ui_LASDiAGUI):
         """Function to load and plot the data file"""
         
         path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Load Data File", \
-            r"C:\Users\devoto\work\ID27\data\cea_files\Ar", "Data File(*chi *xy)")
+            r"../data/cea_files/Ar", "Data File(*chi *xy)")
+            # r"C:\Users\devoto\work\ID27\data\cea_files\Ar", "Data File(*chi *xy)")
+
         
         self.Q, self.I_Q = Utility.read_file(path)
         
@@ -107,7 +87,8 @@ class LASDiA(QtWidgets.QMainWindow, LASDiAGUI.Ui_LASDiAGUI):
         """Function to load and plot the bkg file"""
         
         path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Load Bkg File", \
-            r"C:\Users\devoto\work\ID27\data\cea_files\Ar", "Data File(*chi *xy)")
+            r"../data/cea_files/Ar", "Data File(*chi *xy)")
+        	# r"C:\Users\devoto\work\ID27\data\cea_files\Ar", "Data File(*chi *xy)")
         
         self.Qbkg, self.Ibkg_Q = Utility.read_file(path)
         
